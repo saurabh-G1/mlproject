e9f8dfff3b2bdfd72e78e9396447715ea3263c7c
saurabh-G1 <gaudsaurabh4@gmail.com>
2025-03-02 14:22:00 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,8 @@ from dataclasses import dataclass  #used for create classvariables i.e. very coo
 from src.components.data_transformation import DataTransformation  
 from src.components.data_transformation import DataTransformationConfig 
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer 
 
 # Whenever we perform data ingestion component their should be some i/p's that may be required by this data ingestion components.  
 #the i/p can be like where i have to save the training part, where i have to save the test part, where i've to save the raw data, 
@@ -62,11 +64,15 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()  
     
     data_transformation=DataTransformation()  
-    data_transformation.initiate_data_transformation(train_data,test_data)   
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)   
+    
+    modeltrainer=ModelTrainer() 
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr)) 
+    
     
     
         
-                
+
     
     
         
